0c1b068e40b6b0875956af912271f80be2d34b12
jmeegan2 <jmeegan8@gmail.com>
2022-09-18 21:09:27 -0400

    working through exercise 1.1, creating components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+  
+
+const App = () => { 
+  const course = 'Half stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = ' Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'States of a component'
+  const exercises3 = 14
+  
+  // Header component
+
+  const Header = (props) => {
+    return (
+      <div>
+        <h1>
+      {props.course}
+      </h1>
+      </div>
+      
+    )
+    }
+
+    const Content = (props) => {
+      return (
+       <p> 
+        props.
+       </p>
+        
+      )
+      }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div> 
+      <Header course = {course}></Header>
+      <p> 
+        {part1} {exercises1}
+      </p>
+      <p> 
+        {part2} {exercises2}
+      </p>
+      <p> 
+        {part3} {exercises3}
+      </p>
+      <p>Number of Exercises {exercises1 + exercises2 + exercises3} </p>
     </div>
-  );
+    </>
+  )
 }
-
 export default App;
